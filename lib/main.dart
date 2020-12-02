43fabb14da3d67bee6bac008319b31301c22dbbb
Davi Leal <lealm.davi@gmail.com>
2020-12-02 18:08:33 -0300

    feat: add level

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,14 @@ void main() => runApp(MaterialApp(
       home: NinjaCard(),
     ));
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +21,15 @@ class NinjaCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel++;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
       body: Padding(
           padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
@@ -57,7 +73,7 @@ class NinjaCard extends StatelessWidget {
                 ),
                 SizedBox(height: 10),
                 Text(
-                  '8',
+                  '$ninjaLevel',
                   style: TextStyle(
                     color: Colors.amberAccent[200],
                     letterSpacing: 2,
