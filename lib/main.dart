d93c14eb8d793200d6e638591b0b4a920c4a5334
Davi Leal <lealm.davi@gmail.com>
2020-12-02 16:55:33 -0300

    feat: profile

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,73 @@ class NinjaCard extends StatelessWidget {
         backgroundColor: Colors.grey[850],
         elevation: 0,
       ),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/thumb.jpg'),
+                    radius: 40,
+                  ),
+                ),
+                Divider(
+                  height: 90,
+                  color: Colors.grey[800],
+                ),
+                Text(
+                  'NAME',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Chun-Li',
+                  style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 30),
+                Text(
+                  'CURRENT NINJA LEVEL',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '8',
+                  style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(width: 10),
+                    Text('chun.li@thenetninja.co.uk',
+                        style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 18,
+                          letterSpacing: 1,
+                        ))
+                  ],
+                ),
+              ])),
     );
   }
 }
